7285e5d7b73986e270b0819394dd332150e0a320
twiw49 <twiw49@gmail.com>
2016-12-17 12:09:31 +0900

    simpler code

--- script.js
+++ script.js
@@ -11,26 +11,16 @@ function lightboxClick(event) {
     var elem = event.target,
         elemID = elem.getAttribute('id'),
         lightboxImg = document.getElementById('lightbox-image'),
-        lightbox = document.getElementById("lightbox-overlay"),
-        newImg = new Image();
+        lightbox = document.getElementById("lightbox-overlay");
 
     // If we click an element with the attribute "data-lightbox", show the lightbox
     if (elem.hasAttribute('data-lightbox')) {
-        event.preventDefault();
-
-        newImg.onload = function() {
-            lightboxImg.src = this.src;
-        }
-
-        lightboxImg.src = '';
-        newImg.src = elem.getAttribute('data-lightbox');
+        lightboxImg.src = elem.getAttribute('data-lightbox');
         lightbox.classList.add('visible');
     }
 
     // If we click any of these 2 elements, close the lightbox
     if (elemID == 'lightbox-image' || elemID == 'lightbox-overlay') {
-        event.preventDefault();
-
         lightbox.classList.remove('visible');
     }
 }
